75ba438d5383df0ce4bd62c47136df5e02a7f946
Slash20 <kurenkov200234@inbox.ru>
2023-05-07 18:50:41 +0300

    Temporarily removed position on TaskModel

--- backend_api/canban/models.py
+++ backend_api/canban/models.py
@@ -5,12 +5,11 @@ class TaskModel(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     board = models.ForeignKey('BoardModel', on_delete=models.CASCADE, related_name='tasks')
-    position = models.PositiveIntegerField(default=0)
+    # position = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['position']
         db_table = 'tasks'
 
     def __str__(self):
