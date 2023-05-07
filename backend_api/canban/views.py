47aa7e7bf7b7721c617074c29f0e7f6324ac8ef5
Slash20 <kurenkov200234@inbox.ru>
2023-05-07 18:51:11 +0300

    Change view

--- backend_api/canban/views.py
+++ backend_api/canban/views.py
@@ -5,38 +5,36 @@ from django.http import HttpResponse
 from canban.serializers import *
 
 
-class MoveTaskView(APIView):
-    def patch(self, request, *args, **kwargs):
-        task_id = kwargs['task_id']
-        task = TaskModel.objects.get(id=task_id)
-        old_board_id = task.boards_id
-        new_board_id = request.data.get('new_boards_id')
-        new_position = request.data.get('new_position')
-        task.boards_id = new_board_id
-        task.position = new_position
-        task.save()
-
-        # Обновляем позицию других задач на старой доске, если они есть
-        if old_board_id != new_board_id:
-            tasks_on_old_board = TaskModel.objects.filter(board_id=old_board_id, position__gte=task.position)
-            for task_on_old_board in tasks_on_old_board:
-                if task_on_old_board.id != task_id:
-                    task_on_old_board.position += 1
-                    task_on_old_board.save()
-
-        # Обновляем позицию других задач на новой доске, если они есть
-        tasks_on_new_board = TaskModel.objects.filter(board_id=new_board_id, position__gte=new_position)
-        for task_on_new_board in tasks_on_new_board:
-            if task_on_new_board.id != task_id:
-                task_on_new_board.position += 1
-                task_on_new_board.save()
-
-        return Response(status=status.HTTP_200_OK)
+# class MoveTaskView(APIView):
+#     def patch(self, request, *args, **kwargs):
+#         tasks_id = kwargs['tasks_id']
+#         task = TaskModel.objects.get(id=tasks_id)
+#         serializer = TaskSerializer(task, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save(new_position=request.data.get('new_position'))
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TasksAPIView(generics.ListCreateAPIView):
     queryset = TaskModel.objects.all()
     serializer_class = TaskSerializer
+
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+
+    # # Создаем новую задачу с position = 0
+    # task = serializer.save(position=0)
+    #
+    # # Обновляем position всех задач на доске, у которых position >= 0
+    # tasks_on_board = TaskModel.objects.filter(board_id=task.board_id, position__gte=0).exclude(id=task.id)
+    # for t in tasks_on_board:
+    #     t.position += 1
+    #     t.save()
+    #
+    # headers = self.get_success_headers(serializer.data)
+    # return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class TasksAPIUpdate(generics.RetrieveUpdateAPIView):
